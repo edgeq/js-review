7406f6f674b2db2d785928906e8e68253e87be08
Edge Quintanilla <edgarq86@gmail.com>
2019-01-07 08:20:03 -0600

    played with logging different data types

---
+++ 02-JS_BASICS-CODEALONG/app.js
@@ -0,0 +1,40 @@
+/** 
+ * DATA TYPES - two types of data types
+ * 
+ * PRIMITIVE vs REFERENCE
+ * 
+ * PRIMITVE = STACK
+ * accessed by its actual value in variable
+ *  - string
+ *  - number (integers, decimals, floats)
+ *  - boolean
+ *  - null (intentionally empty value)
+ *  - undefined
+ *  - symbols 
+ * 
+ * REFERENCE = HEAP
+ * data not stored in variable, but by reference. considered objects
+ *  - arrays
+ *  - object literals
+ *  - functions
+ *  - dates
+ * 
+ * JS IS A DYNAMICALLY TYPED LANGUAGE
+ *  - variables can hold any type of data and be re-assigned to any type of data
+ *  - variables do not need to be contricted to a data type.
+ * 
+ */
+const typedVar = 'this is a string';
+console.log(typeof typedVar);
+
+const typedVar2 = 3999;
+console.log(typeof typedVar2);
+
+const typedVar3 = ['234', 345, [1, 2, 3]];
+console.log(typeof typedVar3);
+
+const typedVar4 = {
+    type: 'object',
+    literal: true
+}
+console.log(typeof typedVar4);

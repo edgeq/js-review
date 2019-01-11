3e01cc6c4b9dedc50723a2844799f805b834ee04
Edge Quintanilla <edgarq86@gmail.com>
2019-01-11 17:29:53 -0600

    set items in local storage and did some type switching to work with localstorage rules

--- 03-DOM+JS-CODEALONG/app.js
+++ 03-DOM+JS-CODEALONG/app.js
@@ -1,46 +1,67 @@
 /**
- *  EVENT BUBBLING AND DELEGATION - 
- *  - BUBBLING: When an event happens, it bubbles up through its parent.
- *  - DELEGATION: listen on a parent element and target children to do something. Used when elements are similar
- *  
+ *  LOCAL AND SESSION STORAGE  - browser-based persistence of data
+ *    - LOCAL STORAGE persists so long as you don't clear browser cache/history
+ *    - SESSION STORAGE persists so long as the session is open / browser doesn't close 
+ * 
+ *  - .getItem()
+ *  - .setItem()
+ *  - .removeItem()
+ *  - .clear()
+ * 
+ *  - Saving to local storage requires key:value pairs. 
+ *    - Must be a string. 
+ *    - JSON.stringify() will turn arrays and objects into strings
+ *    - JSON.parse() will turn strings into JSON
  */
 
- // EVENT BUBBLING
- /* Clicking on .card-title also triggers it's parent containers...
+ // SET LOCAL+SESSION STORAGE ITEM
+
+ //set Items
+//  localStorage.setItem('name', 'edge');
+//  localStorage.setItem('age', 'too old');
+// //  sessionStorage.setItem('name', 'Dani');
+// // localStorage.removeItem('name');
+
+// // get Items
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
+
+// //clear items
+// localStorage.clear();
+
+// console.log(name, age);
+
+//Add form values to local storage
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const task = document.getElementById('task').value;
   
-document.querySelector('.card-title').addEventListener('click', function () {
-  console.log('card title');
-});
-document.querySelector('.card-content').addEventListener('click', function () {
-  console.log('card content');
-});
-document.querySelector('.card').addEventListener('click', function () {
-  console.log('card');
-});
+  let tasks; 
 
-document.querySelector('.col').addEventListener('click', function () {
-  console.log('col');
-});
-*/
-
-// this won't work because we need to delegate events
-// const delItem = document.querySelector('.delete-item');
-// delItem.addEventListener('click', deleteItem);
-
-// EVENT DELEGATION - listening on parent and traversing down. More reliable
-const collectionList = document.querySelector('.collection');
-collectionList.addEventListener('click', deleteItem);
-
-function deleteItem(e){
-  // console.log('deleted item');
-  // console.log(e.target);
-  // if (e.target.parentElement.className === 'delete-item secondary-content' ){
-  //   console.log('delete item')
-  // }
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    console.log('delete item');
-    //target parent of e.target.parentElement and remove
-    e.target.parentElement.parentElement.remove();
+  // check if there's something in localStorage
+  if(localStorage.getItem('tasks') === null) {
+    //if not, set tasks to an empty array
+    tasks = [];
+  } else { // if there is something there
+    // get the string in localStorage
+    // set tasks to an array from existing item 
+    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-}
+  //post-if-statement:
+  // push task from submit to tasks array
+  tasks.push(task);
+  
+  // reset localStorage tasks
+  // pass in array converted to string
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+})
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(task => console.log(task));
+console.log(tasks);
+
+
 

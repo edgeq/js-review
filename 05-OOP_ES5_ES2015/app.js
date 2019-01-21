26d0fbe41f20c973eeb2f1bb8429278fea7df083
Edge Quintanilla <edgarq86@gmail.com>
2019-01-21 12:03:47 -0600

    prototypal inheritance with call method

--- 05-OOP_ES5_ES2015/app.js
+++ 05-OOP_ES5_ES2015/app.js
@@ -1,50 +1,42 @@
 /** 
- * Prototypes Explained  
+ * Prototypal Inheritance  
  *  - each object has a prototype.
  *  - the prototype is an object itself
  *  - all objects inherit properties and methods from their prototype
  * 
  */
 
-// Object.prototype
-//new Person() inherits Person.prototype
-// you can also go up the chain from Person to Object.
 //Person Constructor
-function Person(firstName, lastName, dob) {
+function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob);
-    // this.calculateAge = function() {
-    //     const diff = Date.now() - this.birthday.getTime();
-    //     const ageDate = new Date(diff);
-    //     return Math.abs(ageDate.getUTCFullYear() - 1970);
-    // }
 }
 
-//Calculate age - processes data
-Person.prototype.calculateAge = function() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+Person.prototype.greeting = function() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
 }
 
-// Get full name - concatenates data
-Person.prototype.getFullName = function() {
-    return `${this.firstName} ${this.lastName}`;
+const person1 = new Person('John', 'Doe');
+
+console.log(person1.greeting());
+
+// CUSTOMER CONSTRUCTOR
+function Customer(firstName, lastName, phone, membership) {
+    Person.call(this, firstName, lastName);
+    this.phone = phone;
+    this.membership = membership;
 }
+//Inherit Person Prototype methods
+Customer.prototype = Object.create(Person.prototype);
+//Make Customer.prototype return Customer instead of Person
+Customer.prototype.constructor = Customer;
 
-// Gets Married - manipulates data
-Person.prototype.getsMarried = function(newLastName) {
-    this.lastName = newLastName;
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+// Customer greeting
+Customer.prototype.greeting = function() {
+    return `Hello there ${this.firstName} ${this.lastName}. Welcome to our company`;
+
 }
-
-const edge = new Person('Edge', 'Quintanilla', '06-27-1986');
-const dani = new Person('Dani', 'McLaren', '03-05-1987');
-
-console.log(dani);
-console.log(edge.calculateAge());
-console.log(dani.getFullName());
-dani.getsMarried(edge.lastName);
-console.log(dani.getFullName());
-console.log(dani.hasOwnProperty('firstName'));
-console.log(dani.hasOwnProperty('getFullName'));
+console.log(customer1.greeting());

c5af9f6a6f4b9e262446be83d861883442ccad67
Edge Quintanilla <edgarq86@gmail.com>
2019-01-26 13:28:09 -0600

    Chuck Norris joke generator with JS validation

--- 07-ASYNC-JS-AJAX-FETCH-API/app.js
+++ 07-ASYNC-JS-AJAX-FETCH-API/app.js
@@ -1,4 +1,47 @@
 /**
- *  WORKING WITH EXTERNAL APIs
+ *  WORKING WITH EXTERNAL APIs - Chuck Norris
  * 
  */
+
+document.querySelector('.get-jokes').addEventListener('click', loadJokes);
+
+function loadJokes(e) {
+    e.preventDefault();
+    const number = document.querySelector('input[type="number"]').value;
+    // Get number from input field
+    if (number === undefined || number === null || number === '') {
+        document.querySelector('.jokes').innerHTML = '<h3>Chuck Norris needs a number</h3>';
+        return;
+    }
+    console.log(number);
+    // fetch random jokes
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const res = JSON.parse(this.responseText);
+            console.log(res);
+            let output = '';
+            if (res.type === 'success') {
+                res.value.forEach(function(joke) {
+                    output += `
+                    <h3>${joke.joke}</h3>
+                    <hr>
+                    `;
+                });
+            } else {
+                output += '<li>Something went wrong</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
+
+        }
+    };
+    xhr.send();
+
+
+}
+
+// TODO: Redo this using Grid/flexbox

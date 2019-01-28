b3a45f8b8933d01375d3d3d674dc27923a012766
edgeq <edgarq86@gmail.com>
2019-01-28 17:30:02 -0600

    refactored library to use promises

---
+++ 07-ASYNC-JS-AJAX-FETCH-API/08-AJAX_LIB_FETCH_ES6_PROMISES/app.js
@@ -0,0 +1,26 @@
+const http = new EasyHTTP;
+
+//Get Users
+http.get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.table(data))
+  .catch(err => console.log(err));
+
+// POST Request 
+//user data 
+const data = {
+  name: 'Edge',
+  username: 'edgeq',
+  email: 'email@email.com'
+}
+http.post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.table(data))
+  .catch(err => console.log(err));
+
+//UPDATE 
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+  .then(data => console.table(data))
+  .catch(err => console.log(err));
+
+http.delete('https://jsonplaceholder.typicode.com/users/3')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));

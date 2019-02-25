6530cdd56e4203dced1278fe7dfdb1ad06183a5f
Edge Quintanilla <edgarq86@gmail.com>
2019-02-24 21:28:08 -0600

    pull data and forEach into DOM

---
+++ 13-BABEL-WEBPACK/src/ui.js
@@ -0,0 +1,28 @@
+class UI {
+  constructor(){
+    this.posts = document.querySelector('#posts');
+    this.titleInput = document.querySelector('#title');
+    this.bodyInput = document.querySelector('#body');
+    this.idInput = document.querySelector('#id');
+    this.postSubmit = document.querySelector('.post-submit');
+    this.formState = 'add';
+  }
+
+  showPosts(posts){
+    console.log(posts);
+    let output = '';
+    posts.forEach((post) => {
+      output += `
+      <div class="card mb-3">
+        <div class="card-body">
+          <h4 class="card-title>${post.title}</h4>
+          <p class="card-text">${post.body}</p>
+        </div>
+      </div>
+      `;
+    });
+    this.posts.innerHTML = output;
+  }
+}
+
+export const ui = new UI();
